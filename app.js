3ba54639333064a30d7493539d898597396cb120
Zacchaeus Napuo <2016.naaz@gmail.com>
2020-12-06 19:01:11 +0530

    Add Google and Facebook login

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const passportLocalMongoose = require("passport-local-mongoose");
 //passport-local is a dependency required by passport-local-mongoose
 //but it is not necesary to refer/expose it in the code.
 
+//3RD PARTY OAUTH STEP 1
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+// const LinkedInStrategy = require("passport-linkedin-oauth2").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 //level 5&6 authentication
 
 const app = express();
@@ -27,7 +32,7 @@ app.use(express.static("public"));
 //STEP 1
 app.use(
   session({
-    secret: "process.env.SECRET",
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false,
   })
@@ -47,32 +52,110 @@ mongoose.connect(process.env.DATABASE_URL, {
 
 //schemas
 const userSchema = new Schema({
-  username: {
-    type: String,
-    required: [true, "An username is required!"],
-  },
-  password: {
-    type: String,
-  },
+  username: String,
+  password: String,
+  googleId: String,
+  facebookId: String,
+  secret: String,
 });
 
 //STEP 3
 userSchema.plugin(passportLocalMongoose);
+
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 //STEP 4
 passport.use(User.createStrategy());
 
-//STEP 5
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+//STEP 5 --- both local and 3rd party auth
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
 
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+//3RD PARTY OAUTH STEP 2--- Implementing 3rd party authentication using google
+//*************GOOGLE****************** */
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      //   console.log(profile);
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
+
+//**************FACEBOOK*************** */
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: "http://localhost:3000/auth/facebook/secrets",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
+
+//**************LINKEDIN*************** */
+
+//**************GITHUB*************** */
 // routing
 app.route("/").get(function (req, res) {
   res.render("home");
 });
 
+//goolge login redirect route Starts
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect to secrets.
+    res.redirect("/secrets");
+  }
+);
+//goolge login redirect route ends
+
+//facebook login redirect route starts
+app.get("/auth/facebook", passport.authenticate("facebook"));
+
+app.get(
+  "/auth/facebook/secrets",
+  passport.authenticate("facebook", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  }
+);
+//facebook login redirect route ends
+//LINKEDIN login redirect route starts
+
+//LINKEDIN login redirect route ends
+//GITHUB login redirect route starts
+//GITHUB login redirect route ends
 app
   .route("/login")
   .get(function (req, res) {
@@ -124,12 +207,42 @@ app
   });
 
 app.route("/secrets").get(function (req, res) {
-  if (req.isAuthenticated()) {
-    res.render("secrets");
-  } else {
-    res.redirect("/login");
-  }
+  User.find({ secret: { $ne: null } }, function (err, foundUsers) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("secrets", { usersWithSecrets: foundUsers });
+      }
+    }
+  });
 });
+
+app
+  .route("/submit")
+  .get(function (req, res) {
+    if (req.isAuthenticated()) {
+      res.render("submit");
+    } else {
+      res.redirect("/login");
+    }
+  })
+  .post(function (req, res) {
+    const submittedSecret = req.body.secret;
+    // console.log(req.user._id);
+    User.findById(req.user._id, function (err, foundUser) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUser) {
+          foundUser.secret = submittedSecret;
+          foundUser.save(function () {
+            res.redirect("/secrets");
+          });
+        }
+      }
+    });
+  });
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
